18634c6189932b4f5373f3b89bcb3782b6d92ad8
Dor Alon <noalon2628@gmail.com>
2024-06-09 16:18:24 +0300

    write entries to DB

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import os
 from os.path import dirname, abspath
 
+import psycopg2
 import streamlit as st
 
-from display_utils import set_background, set_form_color, set_rtl
+from display_utils import set_background, set_rtl
 
 
 def validate_non_empty_str(text):
@@ -12,15 +13,24 @@ def validate_non_empty_str(text):
     return True
 
 
-def validate_inputs(name, surname, adults_count, children_count):
+def validate_inputs(name, surname, adults_count, children_count, arriving):
     if not validate_non_empty_str(name):
         st.warning('שם חסר')
     if not validate_non_empty_str(surname):
         st.warning('שם משפחה חסר')
 
+    conn = psycopg2.connect(dbname=os.environ['db_name'], user=os.environ['db_user'],
+                            password=os.environ['db_pass'], host=os.environ['db_host'],
+                            port=os.environ['db_port'])
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO guests (name, surname, adults_count, children_count, arriving) "
+                   f"VALUES ('{name}', '{surname}', {adults_count}, {children_count}, {arriving})")
+    conn.commit()
+    cursor.close()
+    conn.close()
+
 
 def create_form():
-    # with st.form("אישור הגעה"):
     name = st.text_input("שם פרטי")
     surname = st.text_input("שם משפחה")
     arriving = st.checkbox("מגיעים?", value=True, key='arriving_toggle')
@@ -35,7 +45,7 @@ def create_form():
 
     st.button('אישור', on_click=validate_inputs,
               kwargs={'name': name, 'surname': surname, 'adults_count': adults_count,
-                      'children_count': children_count}, disabled=not(name and surname))
+                      'children_count': children_count, 'arriving': arriving}, disabled=not(name and surname))
 
 
 if __name__ == '__main__':
